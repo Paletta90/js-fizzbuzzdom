2b0c6ce766cf98029762b4e7f941753477a0b575
DavideDiPietro <davide.dipietro3@gmail.com>
2022-03-11 17:38:35 +0100

    Faccio 7 colonne

--- main.js
+++ main.js
@@ -8,23 +8,23 @@ for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0 && i % 5 == 0) {
         // Stampo il box`
         container.innerHTML += `
-        <div class="col"><div id="fizzBuzz" class="box d-flex justify-content-center align-items-center mb-5">FizzBuzz</div></div>
+        <div class="sevenColumn d-flex justify-content-center"><div id="fizzBuzz" class="box d-flex justify-content-center align-items-center mb-5">FizzBuzz</div></div>
         `
     }
     // Divisibile solo per 3 
     else if (i % 3 == 0) {
         container.innerHTML += `
-        <div class="col"><div id="fizz" class="box d-flex justify-content-center align-items-center mb-5">Fizz</div></div>
+        <div class="sevenColumn d-flex justify-content-center"><div id="fizz" class="box d-flex justify-content-center align-items-center mb-5">Fizz</div></div>
         `
     }
     // Divisibile solo per 5
     else if (i % 5 == 0) {
         container.innerHTML += `
-        <div class="col"><div id="buzz" class="box d-flex justify-content-center align-items-center mb-5">Buzz</div></div>
+        <div class="sevenColumn d-flex justify-content-center"><div id="buzz" class="box d-flex justify-content-center align-items-center mb-5">Buzz</div></div>
         `
     } else {
         container.innerHTML += `
-        <div class="col"><div id="n" class="box d-flex justify-content-center align-items-center mb-5">${i}</div></div>
+        <div class="sevenColumn d-flex justify-content-center"><div id="n" class="box d-flex justify-content-center align-items-center mb-5">${i}</div></div>
         `
     }
 }
