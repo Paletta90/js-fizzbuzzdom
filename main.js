8e3e8376db264d815b50e7bc2cde69b5a2920c35
DavideDiPietro <davide.dipietro3@gmail.com>
2022-03-11 16:22:47 +0100

    Realizzo Millestone 2

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
+let container = document.getElementById("contenitoreBox");
+
 for (let i = 1; i <= 100; i++) {
 
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log("FizzBuzz")
+        container.innerHTML += `
+        <div class="col"><div class="box d-flex justify-content-center align-items-center mb-5">FizzBuzz</div></div>
+        `
+        // console.log("FizzBuzz")
     } else if (i % 3 == 0) {
-        console.log("Fizz")
+        container.innerHTML += `
+        <div class="col"><div class="box d-flex justify-content-center align-items-center mb-5">Fizz</div></div>
+        `
+        // console.log("Fizz")
     } else if (i % 5 == 0) {
-        console.log("Buzz");
+        container.innerHTML += `
+        <div class="col"><div class="box d-flex justify-content-center align-items-center mb-5">Buzz</div></div>
+        `
+        // console.log("Buzz");
     } else {
-        console.log(i);
+        container.innerHTML += `
+        <div class="col"><div class="box d-flex justify-content-center align-items-center mb-5">${i}</div></div>
+        `
+        // console.log(i);
     }
 }
